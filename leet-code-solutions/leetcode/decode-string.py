04b689cf79c5d90aa3f5d5f7111cd6048ae55ae6
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-02-27 14:52:46 +0300

    Add solution for Decode String

--- leet-code-solutions/leetcode/decode-string.py
+++ leet-code-solutions/leetcode/decode-string.py
@@ -1,48 +1,40 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        strStk = []
-        intStk = []
-        i = 0
-        ans = ""
-
-        while i<len(s):
-            count = ""
-            if s[i].isnumeric():
-                while s[i].isnumeric():
-                    count+=s[i]
-                    i+=1
-                intStk.append(int(count))
-            if s[i] == "[":
-                if s[i-1].isnumeric():
-                    strStk.append(s[i])
-                else:
-                    strStk.append(s[i])
-                    intStk.append(1)
-                print(strStk)
-            elif s[i] == "]":
-                res = ""
-                temp = ""
-                tempcount = 0
-                if intStk:
-                    tempcount = intStk.pop()
-                while strStk and strStk[-1] != "[":
-                    temp = strStk.pop() + temp
-
-                if strStk and strStk[-1] == "[":
-                    strStk.pop()
-
-                for _ in range(tempcount):
-                    res+=temp
-                print(res)
-                for j in range(len(res)):
-                    strStk.append(res[j])
-                
-                res = ""
-            else:
-                strStk.append(s[i])
-            i+=1
-
-        while strStk:
-            ans = strStk.pop() + ans
+        
+        def ans(s):
+            i = 0
+            res = []
+            p = 0
             
-        return ans
+            while i < len(s):
+                num = ""
+                if s[i].isnumeric():
+                    ch = ""
+                    while s[i].isnumeric():
+                        num+=s[i]
+                        i+=1
+                    p = 1
+                    num = int(num)
+                    i+=1
+                    while p!=0:
+                        if s[i] == "[":
+                            p+=1
+                            ch+=s[i]
+                        elif s[i] == "]":
+                            p-=1
+                            if p!=0:
+                                ch+=s[i]
+                        else:
+                            ch+=s[i]
+                        i+=1
+                    res.append(ans(ch) * num)
+                else:
+                    res.append(s[i])
+                    i+=1
+
+            return "".join(res)
+                    
+
+
+            
+        return ans(s)
