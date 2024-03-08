22c06777755c22016f1e90f1a115b9a49da7602b
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-03-08 10:50:31 +0300

    Add solution for Find Right Interval

--- leet-code-solutions/leetcode/find-right-interval.py
+++ leet-code-solutions/leetcode/find-right-interval.py
@@ -1,13 +1,13 @@
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
         st_pts = [intervals[i][0] for i in range(len(intervals))]
-        # st_pts_dicto = {i:st_pts[i] for i in range(len(intervals))}
+        st_pts_dicto = {st_pts[i]:i for i in range(len(intervals))}
 
         sorted_st = sorted(st_pts)
 
         ans = []
         for interval in intervals:
             pos = bisect_left(sorted_st,interval[1])
-            ans.append(st_pts.index(sorted_st[pos]) if pos < len(intervals) else -1)
+            ans.append(st_pts_dicto[sorted_st[pos]] if pos < len(intervals) else -1)
         
         return ans
