d9d9f5d1c20eea608e104a0e2ced2b9995d07304
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-08-14 19:59:10 +0300

    Created 'F - Post-war Games' by A2SV Hub

---
+++ A2SV G5 - Contest #9 | Adwa Special! 14-Aug-2024/F - Post-war Games 191526.py
@@ -0,0 +1,19 @@
+# Problem: F - Post-war Games - https://codeforces.com/gym/513152/problem/F
+
+def dmslinp(datatype):
+    return datatype(input())
+def dmsllinp(datatype,itertype = list):
+    return itertype(map(datatype,input().split()))
+def dmslsplit(inp,datatype=str,listtype = list):
+    inp = str(inp)
+    return listtype(map(datatype,inp))
+
+def solved():
+    n,k = dmsllinp(int)
+    if k==n:
+        return 0
+    return (n-(k+1))*3 + ((k-1)//2)*3 + (k+1)%2 + 1
+for _ in range(dmslinp(int)):
+    print(solved())
+    
+
