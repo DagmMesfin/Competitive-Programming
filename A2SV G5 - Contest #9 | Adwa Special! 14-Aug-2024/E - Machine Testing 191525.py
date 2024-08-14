b44bd3730710d677c07fd66dcc18ed576078e2c4
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-08-14 19:59:06 +0300

    Created 'E - Machine Testing' by A2SV Hub

---
+++ A2SV G5 - Contest #9 | Adwa Special! 14-Aug-2024/E - Machine Testing 191525.py
@@ -0,0 +1,38 @@
+# Problem: E - Machine Testing - https://codeforces.com/gym/513152/problem/E
+
+
+from math import ceil
+
+
+def dmslinp(datatype):
+    return datatype(input())
+def dmsllinp(datatype,itertype = list):
+    return itertype(map(datatype,input().split()))
+def dmslsplit(inp,datatype=str,listtype = list):
+    inp = str(inp)
+    return listtype(map(datatype,inp))
+
+for _ in range(int(input())):
+    n = dmslinp(int)
+    h = dmsllinp(int)
+    p = dmsllinp(int)
+    stack = [(float('inf'),p[0])]
+    res = 0
+
+    for i in range(1,n):
+        duration = 0
+        damage = stack[-1][-1] * (stack[-1][0] - duration)
+        while h[i] - damage > 0:
+            t,pp = stack.pop()
+            h[i] -= pp*(t-duration)
+            duration += t-duration
+            damage = stack[-1][-1] * (stack[-1][0] - duration)
+        duration += ceil(h[i]/stack[-1][-1])
+        stack.append((duration,p[i]))
+        res = max(res,duration)
+    
+    print(res)
+
+
+
+
