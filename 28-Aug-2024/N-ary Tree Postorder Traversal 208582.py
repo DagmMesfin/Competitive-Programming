2ed7eada1a6d53fcab64c0ba7e1c10bf8c34a8ec
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-08-28 12:48:20 +0300

    Created 'N-ary Tree Postorder Traversal' by A2SV Hub

---
+++ 28-Aug-2024/N-ary Tree Postorder Traversal 208582.py
@@ -0,0 +1,25 @@
+# Problem: N-ary Tree Postorder Traversal - https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        
+        stack = [root]
+        output = []
+        
+        while stack:
+            node = stack.pop()
+            output.append(node.val)
+            if node.children:
+                stack.extend(node.children)
+        
+        return output[::-1]
