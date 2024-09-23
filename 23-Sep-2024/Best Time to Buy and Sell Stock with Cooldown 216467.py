d29b1eadfc455b7cae2ae2d36615f1716fe213e3
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-09-23 23:58:46 +0300

    Created 'Best Time to Buy and Sell Stock with Cooldown' by A2SV Hub

---
+++ 23-Sep-2024/Best Time to Buy and Sell Stock with Cooldown 216467.py
@@ -0,0 +1,17 @@
+# Problem: Best Time to Buy and Sell Stock with Cooldown - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
+
+        s1 = -prices[0]    
+        s2 = 0              
+        s0 = [0, 0]  
+
+        for p in prices:
+            s1 = max(s1, s0[1] - p)
+            s2 = max(s2, s1 + p)
+            s0 = [s2, s0[0]]
+            
+        return s2
