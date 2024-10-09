f53a69a75eb90642971f39a53549da5188600636
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-10-09 14:53:07 +0300

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 09-Oct-2024/Ehab Is an Odd Person 218992.py
@@ -0,0 +1,32 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+import sys
+input = lambda: sys.stdin.readline().strip()
+def dmslinp(datatype = str):
+    return datatype(input())
+def dmsllinp(datatype,itertype = list):
+    return itertype(map(datatype,input().split()))
+def dmslsplit(inp,datatype=str,listtype = list):
+    inp = str(inp)
+    return listtype(map(datatype,inp))
+
+
+def solve():
+    n = dmslinp(int)
+    arr = dmsllinp(int)
+    isdiff = False
+    for i in range(n-1):
+        if arr[i]%2 != arr[i+1]%2:
+            isdiff = True
+            break
+    
+    if isdiff:
+        arr.sort()
+
+
+    
+    return arr
+
+
+for _ in range(1):
+    print(*solve())
