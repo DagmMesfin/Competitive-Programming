71847017c20e74747b2b411a302545c65188c719
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-10-19 05:13:10 +0300

    Created 'K-th Smallest in Lexicographical Order' by A2SV Hub

---
+++ 19-Oct-2024/K-th Smallest in Lexicographical Order 220724.py
@@ -0,0 +1,25 @@
+# Problem: K-th Smallest in Lexicographical Order - https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/
+
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        current = 1
+        k -= 1
+        
+        while k > 0:
+            count = 0
+            first = current
+            last = current + 1
+            
+            while first <= n:
+                count += min(n + 1, last) - first
+                first *= 10
+                last *= 10
+            
+            if count <= k:
+                current += 1
+                k -= count
+            else:
+                current *= 10
+                k -= 1
+        
+        return current
