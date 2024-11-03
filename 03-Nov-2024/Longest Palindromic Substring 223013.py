f161847aa05a7d212edb12455cc671c5fb031da4
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-11-03 22:43:43 +0300

    Created 'Longest Palindromic Substring' by A2SV Hub

---
+++ 03-Nov-2024/Longest Palindromic Substring 223013.py
@@ -0,0 +1,15 @@
+# Problem: Longest Palindromic Substring - https://leetcode.com/problems/longest-palindromic-substring/
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n < 2: return s
+        dp, ans = [[0]*n for _ in range(n)], {}
+        for i in range(n-1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and ((j - i + 1) <= 3 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+                    ans[j-i+1] = s[i:j+1]
+                else:
+                    dp[i][j] = False
+        return ans[max(ans)]
