86ebfb95051c26727e8dc659a98a6ed3a7591090
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-12-09 01:39:12 +0300

    Created 'Two Best Non-Overlapping Events' by A2SV Hub

---
+++ 08-Dec-2024/Two Best Non-Overlapping Events 224675.py
@@ -0,0 +1,17 @@
+# Problem: Two Best Non-Overlapping Events - https://leetcode.com/problems/two-best-non-overlapping-events/
+
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        events.sort()
+        pq = [] 
+        max_value = 0
+        ans = 0
+        
+        for start, end, value in events:
+            while pq and pq[0][0] < start:
+                max_value = max(max_value, heappop(pq)[1])
+
+            ans = max(ans, max_value + value)
+            heappush(pq, (end, value))
+        
+        return ans
